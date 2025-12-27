ebf8ef05f7e95ab718c79c2ab03aed1ae198fc41
rahul <rajeshreddysura75713@gmail.com>
2025-12-27 20:15:55 +0530

    mails still not sending and upgrade plan issue after trial ends

--- src/components/SubscriptionModal.js
+++ src/components/SubscriptionModal.js
@@ -54,7 +54,7 @@ function SubscriptionModal({ isOpen, onClose }) {
     };
 
     return (
-        <div className="fixed inset-0 z-[10000] flex items-center justify-center p-4">
+        <div className="fixed inset-0 flex items-center justify-center p-4" style={{ zIndex: 10000 }}>
             {/* Backdrop */}
             <div
                 className="absolute inset-0 bg-black bg-opacity-70 backdrop-blur-sm"
